8a3d4f3c5df9e00c2a0021f1a4a0d8a6c1f81618
MUHAMMAD MAMDUH SAFFIN <moesaffin@gmail.com>
2026-09-13 11:35:37 +0800

    Add viewing tip to main seller page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { GccMarketEntryGuide } from "@/components/gcc-market-entry-guide"
+import { ViewingExperienceNotice } from "@/components/viewing-experience-notice"
 
 export default function Page() {
-  return <GccMarketEntryGuide lang="en" />
+  return (
+    <>
+      <GccMarketEntryGuide lang="en" />
+      <ViewingExperienceNotice lang="en" />
+    </>
+  )
 }
